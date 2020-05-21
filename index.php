d2c194da9383bbdae162508ce218300feda917e3
rafamelobin <rafamelobin@gmail.com>
2020-05-21 00:26:02 -0300

    colocando os arquivos de pessoa pdo e modificando readme

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Pessoa PDO</title>
+  <link href="style.css" type="text/css" rel="stylesheet">
+
+</head>
+<body>
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+</body>
+</html>
